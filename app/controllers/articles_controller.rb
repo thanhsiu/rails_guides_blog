1a03b2a965bc06488f9a411d0e6aad2e8547e986
thea <theasp1001@gmail.com>
2020-07-17 17:57:27 +0700

    Adding a Second Model: comment

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -19,7 +19,8 @@ class ArticlesController < ApplicationController
         @article = Article.new(article_params)
 
         if @article.save
-            redirect_to @article
+            # redirect_to @article
+            redirect_to article_path(@article)
         else
             render 'new'
         end
